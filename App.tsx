98326befd287f682a3794a78e826d65f2b53d8f6
JeshuaBen <jeshuacostafer@gmail.com>
2022-08-21 16:25:29 -0300

    feat: creating the setup of the project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import { Welcome } from './src/screens/Welcome';
+import React, { useCallback } from "react";
+
+import { Welcome } from "./src/screens/Welcome";
+
+import * as SplashScreen from "expo-splash-screen";
+
+import theme from "./src/styles/theme";
+
+import {
+  useFonts,
+  Jost_300Light,
+  Jost_400Regular,
+  Jost_500Medium,
+  Jost_600SemiBold
+ } from '@expo-google-fonts/jost';
+import { ThemeProvider } from "styled-components";
 
 export const App = () => {
+  const [fontsLoaded] = useFonts({
+    Jost_300Light,
+    Jost_400Regular,
+    Jost_500Medium,
+    Jost_600SemiBold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return(
-    <Welcome />
+    <ThemeProvider theme={theme}>
+      <Welcome />
+    </ThemeProvider>
   )
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    green50: "#DAF2E4",
+    green600: "#32B768",
+    green800: "#2B7A4B",
+
+    heading: "#52665A",
+    greenBody: "#5C6660",
+    body: "#AAB3AD",
+
+    shape: "linear-gradient(113.01deg, #F5FAF7 0%, #F0F0F0 100%)",
+    background: "#FFFFFF"
+  },
+
+  fonts: {
+    jost_300: "Jost_300Light",
+    jost_400: "Jost_400Regular",
+    jost_500: "Jost_500Medium",
+    jost_600: "Jost_600SemiBold"
+  }
+}
